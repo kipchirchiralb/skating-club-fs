07094b32e03301822fc688c13df6252a6e497883
Albert Kipchirchir <albertkipchirchir02@gmail.com>
2025-07-09 16:26:09 +0300

    sesion man

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mysql = require("mysql");
+const session = require("express-session");
 
 const dbConnection = mysql.createConnection({
   host: "localhost",
@@ -19,20 +20,59 @@ dbConnection.connect((error) => {
 
 const app = express();
 
+// middleware to parse the body of the request
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "uglify",
+    resave: false,
+  })
+);
+app.use((req, res, next) => {
+  console.log(req.session.user);
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("Home/Dashboard page - Default/Root/Home route");
 });
 
 app.get("/members", (req, res) => {
+  if (req.session.user) {
+    console.log("User is logged in:", req.session.user.username);
+  } else {
+    return res.redirect("/login");
+  }
   dbConnection.query("select * from members", (error, results) => {
     res.render("index.ejs", { members: results });
   });
 });
 
+// Auth routes
+app.get("/login", (req, res) => {
+  // console.log(req);
+  res.render("login.ejs");
+});
+
+app.post("/login", (req, res) => {
+  console.log(req.body.username, req.body.password);
+  dbConnection.query(
+    "SELECT * FROM admins WHERE username = ? AND password = ?",
+    [req.body.username, req.body.password],
+    (error, results) => {
+      console.log(results);
+      if (results.length === 0) {
+        return res.status(401).send("Invalid username or password");
+      } else {
+        // Store user info in session
+        req.session.user = results[0];
+        res.send("Login successful");
+      }
+    }
+  );
+});
 // create a route for resources just like the above for members
 // then create a view(ejs file in views folder) to display the data in a table
-
 
 app.get("/activemembers", (req, res) => {
   dbConnection.query(
